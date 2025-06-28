05f049c191496141a8d873e82b5b04a00ae81ede
HabibaAhmedAttia <habibaahmedattia5@gmail.com>
2025-06-28 18:53:04 +0300

    create entities and initialize database

--- services/product/src/main/java/com/ecom/ecommerce/ProductApplication.java
+++ services/product/src/main/java/com/ecom/ecommerce/ProductApplication.java
@@ -1,4 +1,4 @@
-package com.ecom.ecommerce.product;
+package com.ecom.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- services/product/src/test/java/com/ecom/ecommerce/ProductApplicationTests.java
+++ services/product/src/test/java/com/ecom/ecommerce/ProductApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ecom.ecommerce.product;
+package com.ecom.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
